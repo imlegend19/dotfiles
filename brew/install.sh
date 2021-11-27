7f197e413ca28f29b582cb79f0732fd4d4714995
Mahen Gandhi <mahengandhi19@gmail.com>
2021-11-27 21:05:09 +0530

    feat: adds homebrew scripts

---
+++ brew/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  echo "Seting up Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
