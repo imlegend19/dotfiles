fba803b4b262021e614739810f029414fbbc1e3f
Mahen Gandhi <mahengandhi19@gmail.com>
2021-11-27 22:42:36 +0530

    feat: adds global .gitignore

--- git/install.sh
+++ git/install.sh
@@ -5,6 +5,9 @@ echo "Setting up git..."
 # Automatic prune on git fetch/pull
 git config --global fetch.prune true
 
+# Set global .gitignore file
+git config --global core.excludesfile $DOTFILES/git/.gitignore
+
 # Basic git aliases
 alias gpull="git pull"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
